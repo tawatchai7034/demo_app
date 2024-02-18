ccbef71edb64f4cfd5e149e7a2aaa67556db3aa0
tawatchai sunanta <53972621+tawatchai7034@users.noreply.github.com>
2024-02-18 11:15:13 +0700

    sorting success

--- lib/views/customer.view/customer.view.dart
+++ lib/views/customer.view/customer.view.dart
@@ -1,3 +1,4 @@
+import 'package:demo_app/models/customer.models.dart';
 import 'package:demo_app/styles/allDialog.dart';
 import 'package:demo_app/view.models/customer.view.model.dart';
 import 'package:demo_app/views/customer.view/customer.add.dart';
@@ -15,53 +16,85 @@ class CustomerView extends StatefulWidget {
 class _CustomerViewState extends State<CustomerView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Demo App')),
-      body: Consumer<CustomerViewModel>(
-        builder: (context, viewModel, child) {
-          return ListView.builder(
-            itemCount: viewModel.dataList.length,
-            itemBuilder: (context, index) {
-              final customer = viewModel.dataList[index];
-              return Container(
-                color: index % 2 == 0 ? Colors.white : Colors.grey[200],
-                child: ListTile(
-                  title: Text("${customer.cFirstName} ${customer.cLastName}"),
-                  subtitle: Text('Province: ${customer.cProvince}'),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {
-                      Tdialog.infoDialog(
-                          context,
-                          Text(
-                              "Do you want delete ${customer.cFirstName} ${customer.cLastName} ?"),
-                          () {
-                        viewModel.deleteCustomer(customer);
-                        Navigator.of(context, rootNavigator: true).pop("Ok");
-                      }, () {
-                        Navigator.of(context, rootNavigator: true)
-                            .pop("Cancel");
-                      });
-                    },
-                  ),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) =>
-                            CustomerEdit(information: customer)));
-                  },
-                ),
-              );
-            },
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Demo App'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: "First Name (A-Z)",
+              ),
+              Tab(
+                text: "Province (A-Z)",
+              ),
+            ],
+          ),
+        ),
+        body: Consumer<CustomerViewModel>(builder: (context, viewModel, child) {
+          return TabBarView(
+            children: <Widget>[
+              Center(
+                child: dataView(viewModel, "cFirstName"),
+              ),
+              Center(
+                child: dataView(viewModel, "cProvince"),
+              ),
+            ],
           );
-        },
+        }),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const AddCustomerView()));
+          },
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => const AddCustomerView()));
-        },
-      ),
+    );
+  }
+
+  Widget dataView(CustomerViewModel viewModel, String sortBy) {
+    List<CustomerDataModel> dataList = [...viewModel.dataList];
+    switch (sortBy) {
+      case "cFirstName":
+        dataList.sort(((a, b) => a.cFirstName!.compareTo(b.cFirstName!)));
+      case "cProvince":
+        dataList.sort(((a, b) => a.cProvince!.compareTo(b.cProvince!)));
+    }
+    return ListView.builder(
+      itemCount: dataList.length,
+      itemBuilder: (context, index) {
+        final customer = dataList[index];
+        return Container(
+          color: index % 2 == 0 ? Colors.white : Colors.grey[200],
+          child: ListTile(
+            title: Text("${customer.cFirstName} ${customer.cLastName}"),
+            subtitle: Text('Province: ${customer.cProvince}'),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                Tdialog.infoDialog(
+                    context,
+                    Text(
+                        "Do you want delete ${customer.cFirstName} ${customer.cLastName} ?"),
+                    () {
+                  viewModel.deleteCustomer(customer);
+                  Navigator.of(context, rootNavigator: true).pop("Ok");
+                }, () {
+                  Navigator.of(context, rootNavigator: true).pop("Cancel");
+                });
+              },
+            ),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => CustomerEdit(information: customer)));
+            },
+          ),
+        );
+      },
     );
   }
 }
